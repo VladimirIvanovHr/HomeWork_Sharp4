949f31d6240d3e06bd89af7a1cd129b22582cbff
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-16 17:22:44 +0300

    Задача на сумму цифр числа

---
+++ HW_4_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: "); 
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = Convert.ToString(number).Length; 
+int remain = 0;
+int result = 0;
+for (int i = 0; i < counter; i++)
+    {
+      remain = number - number % 10; 
+      result = result + (number - remain); 
+      number = number / 10; 
+    }
+Console.WriteLine("Сумма цифр в числе: " + result);
