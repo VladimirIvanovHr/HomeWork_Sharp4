dfd6990e4ba8480b11593b6872ca27bb272fbfbb
Vladimir Ivanov <ivanofvv9@gmail.com>
2022-10-16 16:22:23 +0300

    Задача на натуранльное число А в степени В

---
+++ HW_4_1/Program.cs
@@ -0,0 +1,13 @@
+﻿ // Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+
+Console.WriteLine("Ввдеите число A ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввдеите число B ");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    } 
+
+Console.WriteLine($"Число {a} в степени {b} равно {result}");
